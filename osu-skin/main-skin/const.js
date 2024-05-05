d8f4d91fe29d0820f037320b79779c7cad72be6d
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2024-05-05 18:44:23 -0500

    Add files via upload

---
+++ osu-skin/main-skin/const.js
@@ -0,0 +1,512 @@
+const template = {
+    id: 'skin_number',
+    name: 'skin_name',
+    date_start: 'date_start',
+    date_end: 'date_end',
+    url: 'url',
+    video_url: 'url',
+    images: [
+        {
+            name: 'image_name',
+            url: 'url',
+            image_url: '01/thing.jpg',
+            isGameplay: false,
+        }
+    ]
+}
+
+const skins = [
+    {
+        id: '01',
+        name: 'Tuyu V2',
+        date_start: '10-19-2022',
+        date_end: 'present',
+        url: 'https://mega.nz/file/rx0V3TzR#ojbWoHPqlifqfJboNeQfJExzv_uStMjGmXiXnMVAt2A',
+        video_url: '_xvM9tE7YoI',
+        images: [
+            {
+                name: 'Gameplay',
+                url: 'https://www.mediafire.com/file/nnq7lb5g295a6a1/-_%2523_TUYU_%2523_-.osk/file',
+                image_url: '1.jpg',
+                isGameplay: true,
+            },
+            {
+                name: 'Song select',
+                url: 'url',
+                image_url: '01/3 - T1JamCd.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Mod select',
+                url: 'url',
+                image_url: '01/2 - kQ6uUz7.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Ranking panel',
+                url: 'url',
+                image_url: '01/5 - JXg00Xs.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Pass',
+                url: 'url',
+                image_url: '01/1 - nbQGwiY.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Fail',
+                url: 'url',
+                image_url: '01/4 - oV1XqFg.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Spinner',
+                url: 'url',
+                image_url: '01/6 - CVFVzjI.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Skip',
+                url: 'url',
+                image_url: '01/7 - X5bITvk.jpg',
+                isGameplay: false,
+            },
+        ]
+    },
+    {
+        id: '02',
+        name: 'Tuyu Instafade V2',
+        date_start: '10-19-2022',
+        date_end: 'present',
+        url: 'https://mega.nz/file/SxUBTQCI#2DUpm1-Bstujn2Au2zkeSgK35gvNEMDYdA-A5BEmEeQ',
+        video_url: 'XozH7aJFAKw',
+        images: [
+            {
+                name: 'Gameplay',
+                url: 'https://www.mediafire.com/file/4grb7htryywi2n6/-_%2523_TUYU_Instafade_%2523_-.osk/file',
+                image_url: '2.jpg',
+                isGameplay: true,
+            },
+            {
+                name: 'Song select',
+                url: 'url',
+                image_url: '02/2 - qww0QNU.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Mod select',
+                url: 'url',
+                image_url: '02/1 - MiMPSx6.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Ranking panel',
+                url: 'url',
+                image_url: '02/3 - n6LHbP6.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Pass',
+                url: 'url',
+                image_url: '02/7 - UWzVVSa.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Fail',
+                url: 'url',
+                image_url: '02/8 - oV1XqFg.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Spinner',
+                url: 'url',
+                image_url: '02/4 - hG9WKIm.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Skip',
+                url: 'url',
+                image_url: '02/5 - RNo0LNr.jpg',
+                isGameplay: false,
+            },
+        ]
+    },
+    {
+        id: '03',
+        name: 'Paraliyzed V23',
+        date_start: '04-29-2024',
+        date_end: 'present',
+        url: 'https://mega.nz/file/C91DTDoA#tig-DQCvveHKrUSpqeSPvvgOY13AKMDbnAlk2hNdtmw',
+        video_url: 'rwj4PRoKLiM',
+        images: [
+            {
+                name: 'Gameplay',
+                url: 'https://mega.nz/file/C91DTDoA#tig-DQCvveHKrUSpqeSPvvgOY13AKMDbnAlk2hNdtmw',
+                image_url: '3.jpg',
+                isGameplay: true,
+            },
+            {
+                name: 'Song select',
+                url: 'url',
+                image_url: '03/3 - xTTgiW6.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Mod select',
+                url: 'url',
+                image_url: '03/2 - Dzf2NnS.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Ranking panel',
+                url: 'url',
+                image_url: '03/1 - SDtkBsV.jpg',
+                isGameplay: false,
+            },
+			            {
+                name: 'Pass',
+                url: 'url',
+                image_url: '03/4 - UWzVVSa.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Fail',
+                url: 'url',
+                image_url: '03/11 - F95URV3.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Spinner',
+                url: 'url',
+                image_url: '03/6 - 0DdBwTZ.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Skip',
+                url: 'url',
+                image_url: '03/5 - cQRvRaD.jpg',
+                isGameplay: false,
+            },
+			            {
+                name: 'Catch',
+                url: 'url',
+                image_url: '03/8 - t8NRu8c.jpg',
+                isGameplay: false,
+            },
+			            {
+                name: 'Mania',
+                url: 'url',
+                image_url: '03/7 - veyiuwU.jpg',
+                isGameplay: false,
+            },
+			            {
+                name: 'Takio',
+                url: 'url',
+                image_url: '03/9 - 854hKHT.jpg',
+                isGameplay: false,
+            },
+        ]
+    },
+    {
+        id: '04',
+        name: 'Paraliyzed DT V1',
+        date_start: '04-29-2024',
+        date_end: 'present',
+        url: 'https://mega.nz/file/a4kVDZ5D#F9kN8LKJNdv-eH1KpXX75hgbZ1Mwq_lWnDKUyqq5_fE',
+        video_url: 'p0BkW-snsIg',
+        images: [
+            {
+                name: 'Gameplay',
+                url: 'https://mega.nz/file/a4kVDZ5D#F9kN8LKJNdv-eH1KpXX75hgbZ1Mwq_lWnDKUyqq5_fE',
+                image_url: '4.jpg',
+                isGameplay: true,
+            },
+            {
+                name: 'Song select',
+                url: 'url',
+                image_url: '04/9 - CEBe9w1.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Mod select',
+                url: 'url',
+                image_url: '04/8 - Dzf2NnS.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Ranking panel',
+                url: 'url',
+                image_url: '04/7 - SDtkBsV.jpg',
+                isGameplay: false,
+            },
+			{
+                name: 'Pass',
+                url: 'url',
+                image_url: '04/10 - UWzVVSa.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Fail',
+                url: 'url',
+                image_url: '04/11 - F95URV3.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Spinner',
+                url: 'url',
+                image_url: '04/2 - fDL1Roq.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Skip',
+                url: 'url',
+                image_url: '04/1 - 19xzotg.jpg',
+                isGameplay: false,
+            },
+			{
+                name: 'Catch',
+                url: 'url',
+                image_url: '04/5 - wB7mkuP.jpg',
+                isGameplay: false,
+            },
+			{
+                name: 'Mania',
+                url: 'url',
+                image_url: '04/4 - t9UbjCt.jpg',
+                isGameplay: false,
+            },
+			{
+                name: 'Takio',
+                url: 'url',
+                image_url: '04/6 - 1Iqlf3L.jpg',
+                isGameplay: false,
+            },
+
+        ]
+    },
+    {
+        id: '05',
+        name: 'Paraliyzed Instafade V4',
+        date_start: '04-29-2024',
+        date_end: 'present',
+        url: 'https://mega.nz/file/T0kgmKbL#w-7EaD8J_f5ToCaKJGXPmFV4ggFKMlt5_dFXo9rOCCM',
+        video_url: 'XUB1Ff9Z0Nk',
+        images: [
+            {
+                name: 'Gameplay',
+                url: 'https://mega.nz/file/T0kgmKbL#w-7EaD8J_f5ToCaKJGXPmFV4ggFKMlt5_dFXo9rOCCM',
+                image_url: '5.jpg',
+                isGameplay: true,
+            },
+            {
+                name: 'Song select',
+                url: 'url',
+                image_url: '05/3 - PfnMu3m.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Mod select',
+                url: 'url',
+                image_url: '05/2 - FSr7iq3.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Ranking panel',
+                url: 'url',
+                image_url: '05/1 - 6yDNg6c.jpg',
+                isGameplay: false,
+            },
+			{
+                name: 'Pass',
+                url: 'url',
+                image_url: '05/4 - YSD2ZBQ.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Fail',
+                url: 'url',
+                image_url: '05/11 - F95URV3.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Spinner',
+                url: 'url',
+                image_url: '05/6 - 0DdBwTZ.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Skip',
+                url: 'url',
+                image_url: '05/5 - cQRvRaD.jpg',
+                isGameplay: false,
+            },
+			{
+                name: 'Catch',
+                url: 'url',
+                image_url: '05/9 - wB7mkuP.jpg',
+                isGameplay: false,
+            },
+			{
+                name: 'Mania',
+                url: 'url',
+                image_url: '05/8 - t9UbjCt.jpg',
+                isGameplay: false,
+            },
+			{
+                name: 'Takio',
+                url: 'url',
+                image_url: '05/10 - 1Iqlf3L.jpg',
+                isGameplay: false,
+            },
+
+        ]
+    },
+    {
+        id: '06',
+        name: 'Paraliyzed With Reedkussy Cursor V6',
+        date_start: '04-29-2024',
+        date_end: 'present',
+        url: 'https://mega.nz/file/P5dRxJDR#Y0nBoi77jtTTUDk168EjoRxQm0UOl4dWX21oZSe6Y0o',
+        video_url: 'T2a7l4HKg0E',
+        images: [
+            {
+                name: 'Song select',
+                url: 'url',
+                image_url: '06/3 - A3yLU7O.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Mod select',
+                url: 'url',
+                image_url: '06/2 - I6iSG4h.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Ranking panel',
+                url: 'url',
+                image_url: '06/1 - vfuOIYY.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Gameplay',
+                url: 'https://mega.nz/file/P5dRxJDR#Y0nBoi77jtTTUDk168EjoRxQm0UOl4dWX21oZSe6Y0o',
+                image_url: '6.jpg',
+                isGameplay: true,
+            },
+			{
+                name: 'Pass',
+                url: 'url',
+                image_url: '06/4 - 2ROjtzz.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Fail',
+                url: 'url',
+                image_url: '06/11 - F95URV3.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Spinner',
+                url: 'url',
+                image_url: '06/6 - 0DdBwTZ.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Skip',
+                url: 'url',
+                image_url: '06/5 - cQRvRaD.jpg',
+                isGameplay: false,
+            },
+			            {
+                name: 'Catch',
+                url: 'url',
+                image_url: '06/9 - wB7mkuP.jpg',
+                isGameplay: false,
+            },
+			            {
+                name: 'Mania',
+                url: 'url',
+                image_url: '06/8 - t9UbjCt.jpg',
+                isGameplay: false,
+            },
+			            {
+                name: 'Takio',
+                url: 'url',
+                image_url: '06/10 - 1Iqlf3L.jpg',
+                isGameplay: false,
+            },
+        ]
+    },
+    {
+        id: '07',
+        name: 'Paraliyzed With WhiteCat Cursor V6',
+        date_start: '04-29-2024',
+        date_end: 'present',
+        url: 'https://mega.nz/file/f5ND1BjY#g6Etqy4ToeG-6uim6qJNr2muFig3aQimeSiJ0wyMkxs',
+        video_url: 'JXrAWDwREJk',
+        images: [
+            {
+                name: 'Gameplay',
+                url: 'https://mega.nz/file/f5ND1BjY#g6Etqy4ToeG-6uim6qJNr2muFig3aQimeSiJ0wyMkxs',
+                image_url: '7.jpg',
+                isGameplay: true,
+            },
+            {
+                name: 'Song select',
+                url: 'url',
+                image_url: '07/3 - CEBe9w1.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Ranking panel',
+                url: 'url',
+                image_url: '07/1 - MjDWJSZ.jpg',
+                isGameplay: false,
+			},
+			{
+                name: 'Pass',
+                url: 'url',
+                image_url: '07/4 - ThHLWQY.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Fail',
+                url: 'url',
+                image_url: '07/11 - F95URV3.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Spinner',
+                url: 'url',
+                image_url: '07/6 - 0DdBwTZ.jpg',
+                isGameplay: false,
+            },
+            {
+                name: 'Skip',
+                url: 'url',
+                image_url: '07/5 - cQRvRaD.jpg',
+                isGameplay: false,
+            },
+			{
+                name: 'Catch',
+                url: 'url',
+                image_url: '07/8 - t8NRu8c.jpg',
+                isGameplay: false,
+            },
+			{
+                name: 'Mania',
+                url: 'url',
+                image_url: '07/7 - t9UbjCt.jpg',
+                isGameplay: false,
+            },
+			{
+                name: 'Takio',
+                url: 'url',
+                image_url: '07/9 - 1Iqlf3L.jpg',
+                isGameplay: false,
+            },
+        ]
+    }
+
+]
