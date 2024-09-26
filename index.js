a6570ab795c5f763f1b2a67e12ebb21f49dcea3b
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2024-09-25 19:45:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,120 +3,128 @@ stuff = {
         class: "discpng",
         link: "https://discord.gg/XVhf65BvUT",
         img: {
-            base: "image/discord.png",
-            hover: "image/discord-h.png",
+            base: "/image/discord.png",
+            hover: "/image/discord-h.png",
         },
     },
     Email: {
         class: "mailpng",
         link: "mailto:business@paraliyzed.com",
         img: {
-            base: "image/mail.png",
-            hover: "image/mail-h.png",
+            base: "/image/mail.png",
+            hover: "/image/mail-h.png",
         },
     },
     Github: {
         class: "gitpng",
         link: "https://github.com/Paraliyzedevo/",
         img: {
-            base: "image/git.png",
-            hover: "image/git-h.png",
+            base: "/image/git.png",
+            hover: "/image/git-h.png",
         },
     },
     Instagram: {
         class: "instagrampng",
         link: "https://www.instagram.com/paraliyzed_evo/",
         img: {
-            base: "image/instagram.png",
-            hover: "image/instagram-h.png",
+            base: "/image/instagram.png",
+            hover: "/image/instagram-h.png",
         },
     },
     Musescore: {
         class: "mailpng",
         link: "https://musescore.com/paraliyzed_evo",
         img: {
-            base: "image/musescore.png",
-            hover: "image/musescore-h.png",
+            base: "/image/musescore.png",
+            hover: "/image/musescore-h.png",
         },
     },
     'My skins': {
         class: "osuskpng",
         link: "osu-skin/HOME.html",
         img: {
-            base: "image/osusk.png",
-            hover: "image/osusk-h.png",
+            base: "/image/osusk.png",
+            hover: "/image/osusk-h.png",
         },
     },
     'osu!': {
         class: "osupng",
         link: "https://osu.ppy.sh/users/19637339",
         img: {
-            base: "image/osu.png",
-            hover: "image/osu-h.png",
+            base: "/image/osu.png",
+            hover: "/image/osu-h.png",
         },
     },
     'Reddit': {
         class: "redditpng",
         link: "https://www.reddit.com/user/Paraliyzed_evo/",
         img: {
-            base: "image/reddit.png",
-            hover: "image/reddit-h.png",
+            base: "/image/reddit.png",
+            hover: "/image/reddit-h.png",
         },
     },
     'Soundcloud': {
         class: "soundcloudpng",
         link: "https://soundcloud.com/bestosuplayer",
         img: {
-            base: "image/soundcloud.png",
-            hover: "image/soundcloud-h.png",
+            base: "/image/soundcloud.png",
+            hover: "/image/soundcloud-h.png",
         },
     },
     'Specs and Stuff': {
         class: "stuffpng",
         link: "stuff.html",
         img: {
-            base: "image/stuff.png",
-            hover: "image/stuff-h.png",
+            base: "/image/stuff.png",
+            hover: "/image/stuff-h.png",
+        },
+    },
+	'Spotify': {
+        class: "spotifypng",
+        link: "https://open.spotify.com/user/andreivillaro",
+        img: {
+            base: "/image/spotify.png",
+            hover: "/image/spotify-h.png",
         },
     },
     'Steam': {
         class: "steampng",
         link: "https://steamcommunity.com/id/Paraliyzed_evo/",
         img: {
-            base: "image/steam.png",
-            hover: "image/steam-h.png",
+            base: "/image/steam.png",
+            hover: "/image/steam-h.png",
         },
     },
     'Tips': {
         class: "tippng",
         link: "https://streamelements.com/paraliyzed_evo/tip",
         img: {
-            base: "image/tip.png",
-            hover: "image/tip-h.png",
+            base: "/image/tip.png",
+            hover: "/image/tip-h.png",
         },
     },
     Twitch: {
         class: "twitchpng",
         link: "https://twitch.tv/Paraliyzed_evo",
         img: {
-            base: "image/twitch.png",
-            hover: "image/twitch-h.png",
+            base: "/image/twitch.png",
+            hover: "/image/twitch-h.png",
         },
     },
     Twitter: {
         class: "mailpng",
         link: "https://twitter.com/Light_Bushi",
         img: {
-            base: "image/twitter.png",
-            hover: "image/twitter-h.png",
+            base: "/image/twitter.png",
+            hover: "/image/twitter-h.png",
         },
     },
     YouTube: {
         class: "mailpng",
         link: "https://www.youtube.com/@BestOsuPlayer",
         img: {
-            base: "image/yt.png",
-            hover: "image/yt-h.png",
+            base: "/image/yt.png",
+            hover: "/image/yt-h.png",
         },
     },
 }
@@ -147,8 +155,8 @@ function setupHomeLinks() {
                 ${key}</p>
             <p><a href="${stuff[key].link}">
             <div class="${stuff[key].class}">
-                <img src="/${stuff[key].img.base}" class=img-nh alt="png bottom" />
-                <img src="/${stuff[key].img.hover}" class=img-h alt="png top" />
+                <img src="${stuff[key].img.base}" class=img-nh alt="png bottom" />
+                <img src="${stuff[key].img.hover}" class=img-h alt="png top" />
             </div>
             </a></p>
         </div>
