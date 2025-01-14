ebb925d171572b06b29d758bdbd000944f4cb98f
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2025-01-13 22:01:42 -0600

    Reorganize the skin folder (for tmw I aint ts doing today)

---
+++ skins/tuyu-skin/run.js
@@ -0,0 +1,252 @@
+const rafisskin = {
+    name: 'Rafis 2018 HDDT',
+    url: 'https://www.mediafire.com/file/phcsum9n4np44wh/Rafis_2018-03-26_HDDT.osk/file',
+    image: 'Rafis2018HDDT.jpg',
+};
+const bluebudgieskinmixv1_1 = {
+    name: 'BlueBudgie Mix v1.1',
+    url: 'https://drive.google.com/file/d/1OmKr9YPWc-o48-v0UwN2a0cok8Kkd8bk/view',
+    image: 'bluebudgiemix1dot1.jpg',
+}
+
+const skins = [
+    {
+        name: 'Bluebudgie',
+        image: 'https://a.ppy.sh/16743093',
+        userLink: 'https://osu.ppy.sh/u/16743093',
+        skins: [
+            bluebudgieskinmixv1_1,
+            {
+                name: 'seungwons_elaina',
+                url: 'https://drive.google.com/file/d/1sO05Vpu0vxcDpEYBDhzfIrGX6HOIAHCq/view',
+                image: 'seungwonselaina.jpg',
+            },
+            {
+                name: 'SaberStrike 1.5',
+                url: 'https://drive.google.com/file/d/1OmKr9YPWc-o48-v0UwN2a0cok8Kkd8bk/view',
+                image: 'SaberStrike1dot5.jpg',
+            },
+            {
+                name: 'SaberStrike 1.2/1.3',
+                url: 'https://www.mediafire.com/file/ouzknrd07gstnrd/',
+                image: 'SaberStrike1dot2.jpg',
+            },
+            rafisskin,
+        ]
+    },
+    {
+        name: 'Bluegamingftw',
+        image: 'https://a.ppy.sh/12387398',
+        userLink: 'https://osu.ppy.sh/u/12387398',
+        skins: [
+            {
+                name: 'SaberStrike DT1.3',
+                url: 'https://drive.google.com/file/d/1V1LAmdeexmpipsZFc9XmQ4BmiMByvUzD/view',
+                image: 'saberstrikedt1dot3.jpg',
+            },
+        ]
+    },
+    {
+        name: 'Byonick',
+        image: 'https://a.ppy.sh/12979064',
+        userLink: 'https://osu.ppy.sh/u/12979064',
+        skins: [
+            {
+                name: 'Byonick 1.1',
+                url: 'https://drive.google.com/file/d/1f_LHTP3FYky9qXp_dAJfdLi7_I3ZhU3m/view',
+                image: 'byonick1dot1.jpg',
+            },
+        ]
+    },
+    {
+        name: 'Duckyboi',
+        image: './img/avatar404.jpg',
+        userLink: '#',
+        skins: [
+            {
+                name: 'MonkoEdit',
+                url: 'https://skins.osuck.net/index.php?newsid=2011',
+                image: 'https://files.osuck.link/images/skins/ab358180380acd6b11f7def15b844b20.webp',
+            },
+            rafisskin,
+        ]
+    },
+    {
+        name: 'Hanoji',
+        image: 'https://a.ppy.sh/18707960',
+        userLink: 'https://osu.ppy.sh/u/18707960',
+        skins: [
+            {
+                name: 'H4N0J1 NM - 2.1',
+                url: 'https://drive.google.com/file/d/1TY82O-vDvhiy5zBhZ2-pcsF5671CckZy/view?usp=sharing',
+                image: 'hanoji2dot1nm.jpg',
+            },
+            {
+                name: 'H4N0J1 HDDT - 2.1',
+                url: 'https://drive.google.com/file/d/1AgJ-xMHPhi1jTEBsYvNWtHqn_QvR4Zl5/view?usp=sharing',
+                image: 'hanoji2dot1dt.jpg',
+            },
+        ]
+    },
+    {
+        name: 'ikugoi',
+        image: 'https://a.ppy.sh/17533279',
+        userLink: 'https://osu.ppy.sh/u/17533279',
+        skins: [
+            {
+                name: 'Skadi Waverider NM',
+                url: 'https://drive.google.com/drive/u/0/mobile/folders/1fMTLeZld5G7NsidkJGsvnW-azQPApFa3?usp=sharing',
+                image: 'BYSkadiWaveriderNM.jpg',
+            },
+            {
+                name: 'Spazza17 Fumo Fade',
+                url: 'https://github.com/Spazza17/Spazza17-Skins/blob/main/spazza17-skins.md#spazza17-fumo-fade',
+                image: 'spazzav2fumofade.jpg',
+            },
+            {
+                name: 'Walnut 1.0 『clrs』',
+                url: 'https://drive.google.com/file/d/1Lgs-Yfyqwx8TjtBDzrPvEoe8YK17e7Pe/view',
+                image: 'vaxeiwalnut1.jpg',
+            },
+            {
+                name: 'Mrekk 3.0 - Aristia',
+                url: 'https://drive.google.com/file/d/1tAKVY6ZQslhOsHlGguqQonnWygK-uC0B/view',
+                image: 'mrekk3aristia.jpg',
+            },
+        ]
+    },
+    {
+        name: 'OniisanBaka',
+        image: 'https://a.ppy.sh/13780464',
+        userLink: 'https://osu.ppy.sh/u/13780464',
+        skins: [
+            {
+                name: 'SaberStrike『0』',
+                url: 'https://www.mediafire.com/file/kjo5xg0egnorauc/-_%2540_SaberStrike_%25E3%2580%258E0%25E3%2580%258FDommisanbaka.osk/file',
+                image: 'saberstrike0oniisan.jpg',
+            },
+        ]
+    },
+    {
+        name: 'radiite',
+        image: 'https://a.ppy.sh/19304434',
+        userLink: 'https://osu.ppy.sh/u/19304434',
+        skins: [
+            {
+                name: 'Spazza17 Fumo Bacon',
+                url: 'https://drive.google.com/file/d/1UFAdS-EbO4GiPQgZS95fcA3fQfq6WYE9/view',
+                image: 'spazzafumobacon.jpg',
+            },
+            {
+                name: 'Rafis HDDT mrekk',
+                url: 'https://github.com/DevenRam/mrekk-skinss#rafis-hddt',
+                image: 'rafishddtmrekk.jpg',
+            },
+        ]
+    },
+    {
+        name: 'Soragaton',
+        image: 'https://a.ppy.sh/16060186',
+        userLink: 'https://osu.ppy.sh/u/16060186',
+        skins: [
+            {
+                name: 'Aristia(edit)+white',
+                url: 'https://drive.google.com/file/d/1-rq_4VAVROH8IbS0gAIMpGcM4sIVU0mR/view',
+                image: 'aristiaeditwhite.jpg',
+            },
+            bluebudgieskinmixv1_1,
+            {
+                name: 'WhiteCat 2.0 old',
+                url: 'https://skins.osuck.net/index.php?newsid=2021',
+                image: 'whitecatckold2.jpg',
+            },
+            {
+                name: 'WhiteCat 1.0 『CK』',
+                url: 'https://skins.osuck.net/index.php?newsid=1107',
+                image: 'whitecatck1.jpg',
+            },
+            {
+                name: 'SaberStrike『1.2SORAGATON』',
+                url: 'https://drive.google.com/file/d/1-pRLecUTaoUNXpHj1TLSMBayvWC09CT2/view',
+                image: 'saberstrikesora.jpg',
+            },
+            rafisskin,
+        ]
+    },
+]
+
+
+
+
+
+function generate(name) {
+    const main = document.getElementById('content');
+    const user = skins.find(x => x.name.toLowerCase() == name.toLowerCase());
+    if (user) {
+        const titleDiv = document.createElement('div');
+        titleDiv.id = 'rSkinName'
+        titleDiv.classList.add('pageTitle')
+        const titleLink = document.createElement('a');
+        titleLink.href = user.userLink;
+        const h1 = document.createElement('h1');
+        h1.id = 'skinNameLink'
+        h1.innerText = user.name + "'s skins";
+        titleLink.append(h1);
+        titleDiv.append(titleLink);
+        main.append(titleDiv);
+
+        const grid = document.createElement('section');
+        grid.className = 'itemGrid';
+        main.append(grid)
+
+        user.skins.forEach(skin => {
+            const section = document.createElement('section');
+            section.className = 'item';
+            const itemname = document.createElement('h2');
+            itemname.className = 'itemName'
+            itemname.innerText = skin.name
+            const a = document.createElement('a');
+            a.href = skin.url;
+            a.target = '_blank';
+            a.className = 'dl';
+            const img = document.createElement('img');
+            img.src = '../img/sbrstrk-r/' + skin.image.replace('.jpg', '_preview.jpg');
+            img.width = 400;
+            img.height = 225;
+            a.append(img);
+            section.append(itemname, a);
+            grid.append(section);
+        });
+    } else {
+        const h2 = document.createElement('h2');
+        h2.innerText = 'Could not find user labelled: ' + name;
+        main.append(h2)
+    }
+}
+
+function all() {
+    const main = document.getElementById('main');
+    skins.forEach(user => {
+        const sect = document.createElement('section');
+        sect.classList.add('item', 'itemProfile');
+        const playerLink = document.createElement('a');
+        playerLink.className = 'itemAsLink';
+        playerLink.href = user.userLink
+        const title = document.createElement('h2');
+        title.classList.add('itemName', 'itemLongName', 'itemAsLink')
+        title.innerText = user.name;
+        const pageLink = document.createElement('a');
+        pageLink.className = 'dl'
+        pageLink.href = './' + user.name.toLowerCase() + '.html';
+        const img = document.createElement('img');
+        img.src = user.image;
+        const v = '256';
+        img.width = v;
+        img.height = v;
+
+        playerLink.append(title);
+        pageLink.append(img);
+        sect.append(playerLink, pageLink);
+        main.append(sect);
+    })
+}
